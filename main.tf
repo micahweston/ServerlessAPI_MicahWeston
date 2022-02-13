73bf15925bc47808b4103ccffbabcd42d3ebd5f7
Micah Weston <micah.weston@gmail.com>
2022-02-13 13:43:37 -0600

    adding github actions automation

--- main.tf
+++ main.tf
@@ -1,3 +1,22 @@
+terraform {
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+    }
+    random = {
+      source = "hashicorp/random"
+    }
+  }
+
+  cloud {
+    organization = "test-micahweston"
+
+    workspaces {
+      name = "serverless-api-micahweston"
+    }
+  }
+}
+
 provider "aws" {
   region = "us-west-2"
 }
