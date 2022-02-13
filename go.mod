8603c4ccf0d22f8f77802d938d01dec572127341
Micah Weston <micah.weston@gmail.com>
2022-02-12 21:44:45 -0600

    Setting up intial git

---
+++ go.mod
@@ -0,0 +1,13 @@
+module github.com/micahweston/ServerlessAPI_MicahWeston
+
+go 1.17
+
+require (
+	github.com/aws/aws-lambda-go v1.28.0
+	github.com/ipinfo/go/v2 v2.6.0
+)
+
+require (
+	github.com/patrickmn/go-cache v2.1.0+incompatible // indirect
+	golang.org/x/sync v0.0.0-20201207232520-09787c993a3a // indirect
+)
---
+++ main.go
@@ -0,0 +1,73 @@
+package main
+
+import (
+	"context"
+	"encoding/json"
+	"log"
+	"net"
+
+	"github.com/aws/aws-lambda-go/events"
+	"github.com/aws/aws-lambda-go/lambda"
+	"github.com/ipinfo/go/v2/ipinfo"
+)
+
+var MY_TOKEN = "e76fe0d5c94275"
+
+type Response struct {
+	IP       string `json:"ipAddress"`
+	HostName string `json:"hostName"`
+	City     string `json:"city"`
+	Region   string `json:"region"`
+	Country  string `json:"country"`
+	Loc      string `json:"location"`
+	Org      string `json:"org"`
+	Postal   string `json:"postal"`
+	Timezone string `json:"timezone"`
+}
+
+func main() {
+	lambda.Start(handleRequest)
+}
+
+func handleRequest(ctx context.Context, request events.APIGatewayProxyRequest) (events.APIGatewayProxyResponse, error) {
+
+	// Check if ipAddress is in queryStringParameters. If not return bad request response.
+	if request.QueryStringParameters["ipAddress"] == "" {
+		return events.APIGatewayProxyResponse{Body: "Invalid request paramaters", StatusCode: 400}, nil
+	}
+	ipAddress := request.QueryStringParameters["ipAddress"]
+
+	if request.HTTPMethod == "GET" {
+		ipAddress = request.QueryStringParameters["ipAddress"]
+		client := ipinfo.NewClient(nil, nil, MY_TOKEN)
+		info, err := client.GetIPInfo(net.ParseIP(ipAddress))
+
+		if err != nil {
+			log.Fatal(err)
+			return events.APIGatewayProxyResponse{Body: "Request failed please contact support", StatusCode: 400}, nil
+		}
+
+		// saving info from ipinfo to struct
+		var ipResponse = Response{
+			IP:       info.IP.String(),
+			HostName: info.Hostname,
+			City:     info.City,
+			Region:   info.Region,
+			Country:  info.Country,
+			Loc:      info.Location,
+			Org:      info.Org,
+			Postal:   info.Postal,
+			Timezone: info.Timezone,
+		}
+
+		// returning json object for get request
+		resp, err := json.Marshal(ipResponse)
+		if err != nil {
+			log.Fatal(err)
+			return events.APIGatewayProxyResponse{Body: "Request failed please contact support.", StatusCode: 400}, nil
+		}
+		return events.APIGatewayProxyResponse{Body: string(resp), StatusCode: 200}, nil
+	} else {
+		return events.APIGatewayProxyResponse{Body: "Invalid HTTP request", StatusCode: 428}, nil
+	}
+}
